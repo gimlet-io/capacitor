19b97541795b5efa529b2d409db03119e9c9bae3
Laszlo Fogas <laszlo@gimlet.io>
2023-10-23 17:30:29 +0200

    Cleanup

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import APIBackend from "./apiBackend";
 import StreamingBackend from "./streamingBackend";
 import CapacitorClient from "./client";
@@ -21,20 +19,7 @@ function App() {
     <APIBackend capacitorClient={capacitorClient} store={store}/>
     <StreamingBackend capacitorClient={capacitorClient} store={store}/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>hello</div>
     </div>
     </>
   );
