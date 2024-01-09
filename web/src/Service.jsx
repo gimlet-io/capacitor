a7e059245c8727f2597eecf23cdb6bc2155115cd
dzsak <sisakadam@gmail.com>
2024-01-09 13:55:47 +0100

    Classname fix

--- web/src/Service.jsx
+++ web/src/Service.jsx
@@ -363,15 +363,15 @@ function Modal(props) {
 
 const SkeletonLoader = () => {
   return (
-    <div class="w-full">
-      <div class="animate-pulse flex space-x-4">
-        <div class="flex-1 space-y-3 py-1">
-          <div class="h-2 bg-slate-700 rounded"></div>
-          <div class="grid grid-cols-3 gap-4">
-            <div class="h-2 bg-slate-700 rounded col-span-2"></div>
-            <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+    <div className="w-full">
+      <div className="animate-pulse flex space-x-4">
+        <div className="flex-1 space-y-3 py-1">
+          <div className="h-2 bg-slate-700 rounded"></div>
+          <div className="grid grid-cols-3 gap-4">
+            <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+            <div className="h-2 bg-slate-700 rounded col-span-1"></div>
           </div>
-          <div class="h-2 bg-slate-700 rounded"></div>
+          <div className="h-2 bg-slate-700 rounded"></div>
         </div>
       </div>
     </div>
