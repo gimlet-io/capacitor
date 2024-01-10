4ef726db074f8e4c5769479c25dcd890eda7a9fa
dzsak <sisakadam@gmail.com>
2024-01-10 14:16:49 +0100

    Classname

--- web/src/Service.jsx
+++ web/src/Service.jsx
@@ -371,23 +371,23 @@ function Modal(props) {
 
 const SkeletonLoader = () => {
   return (
-    <div class="w-full max-w-4xl animate-pulse space-y-3">
-      <div class="h-2 bg-slate-700 rounded w-1/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-2/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-3/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-4/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-4/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-3/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-2/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-1/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-2/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-2/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-1/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-1/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-1/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-1/6"></div>
-      <div class="h-2 bg-slate-700 rounded w-2/5"></div>
-      <div class="h-2 bg-slate-700 rounded w-3/5"></div>
+    <div className="w-full max-w-4xl animate-pulse space-y-3">
+      <div className="h-2 bg-slate-700 rounded w-1/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-2/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-3/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-4/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-4/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-3/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-2/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-1/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-2/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-2/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-1/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-1/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-1/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-1/6"></div>
+      <div className="h-2 bg-slate-700 rounded w-2/5"></div>
+      <div className="h-2 bg-slate-700 rounded w-3/5"></div>
     </div>
   )
 }
