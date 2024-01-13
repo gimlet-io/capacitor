1e523463f5ae35d53604be6b51db31f9a4db7811
Laszlo Fogas <laszlo@gimlet.io>
2024-01-13 16:19:41 +0100

    Unused

--- web/src/Service.jsx
+++ web/src/Service.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import { RevisionWidget, ReadyWidget } from './FluxState';
 import jp from 'jsonpath';
 import { Logs } from './Logs'
