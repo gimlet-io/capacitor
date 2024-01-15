49328d0b757c829e7f511755a283008a97041aa3
Laszlo Fogas <laszlo@gimlet.io>
2024-01-15 11:33:44 +0100

    Footer summary

--- web/src/Footer.js
+++ web/src/Footer.js
@@ -1,6 +1,7 @@
 import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/24/outline';
 import React, { memo, useState, useCallback } from 'react';
 import { GitRepositories, Kustomizations, HelmReleases } from './FluxState';
+import jp from 'jsonpath'
 
 const Footer = memo(function Footer(props) {
   
@@ -21,7 +22,23 @@ const Footer = memo(function Footer(props) {
   return (
     <div aria-labelledby="slide-over-title" role="dialog" aria-modal="true" className={`fixed inset-x-0 bottom-0 bg-neutral-200 border-t border-neutral-300 ${expanded ? 'h-4/5' : 'h-16'}`}>
       <div className={`flex justify-between w-full ${expanded ? '' : 'h-full'}`}>
-        <div className='h-auto w-full cursor-pointer' onClick={() => setExpanded(!expanded)} />
+        <div
+          className='h-auto w-full cursor-pointer px-16 py-4 flex gap-x-12'
+          onClick={() => setExpanded(!expanded)} >
+          { !expanded &&
+          <>
+          <div>
+            <Summary resources={fluxState.gitRepositories} label="SOURCES" />
+          </div>
+          <div>
+            <Summary resources={fluxState.kustomizations} label="KUSTOMIZATIONS" />
+          </div>
+          <div className="col-span-4">
+            <Summary resources={fluxState.helmReleases} label="HELM-RELEASES" />
+          </div>
+          </>
+          }
+        </div>
         <div className='px-4 py-2'>
           <button
             onClick={() => setExpanded(!expanded)}
@@ -37,9 +54,9 @@ const Footer = memo(function Footer(props) {
             <div className="w-48 px-4 border-r border-neutral-300">
               <SideBar
                 navigation={[
+                  { name: 'Sources', href: '#', count: fluxState.gitRepositories.length },
                   { name: 'Kustomizations', href: '#', count: fluxState.kustomizations.length },
                   { name: 'Helm Releases', href: '#', count: fluxState.helmReleases.length },
-                  { name: 'Sources', href: '#', count: fluxState.gitRepositories.length },
                   { name: 'Flux Runtime', href: '#' },
                 ]}
                 selectedMenu={handlerSelect}
@@ -69,12 +86,42 @@ const Footer = memo(function Footer(props) {
   )
 })
 
+function Summary(props) {
+  const { resources, label } = props;
+
+  if (!resources) {
+    return null;
+  }
+
+  const totalCount = resources.length
+  const readyCount = resources.filter(gitRepository => {
+    const readyConditions = jp.query(gitRepository.status, '$..conditions[?(@.type=="Ready")]');
+    const ready = readyConditions.length === 1 && readyConditions[0].status === "True" 
+    return ready
+  }).length
+
+  const ready = readyCount === totalCount 
+  const readyLabel = ready ? "Ready" : "Not Ready"
+
+  return (
+    <>
+    <div>
+      <span className="font-bold text-neutral-700">{label}:</span>
+      <span className='relative text-neutral-700 ml-5'>
+        <span className="absolute -left-4 top-1 rounded-full h-3 w-3 bg-teal-400 inline-block"></span>
+        <span>{readyLabel}</span>
+      </span>
+      <span>({readyCount}/{totalCount})</span>
+    </div>
+    </>
+  )
+}
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 function SideBar(props) {
-
   const { navigation, selectedMenu, selected } = props;
 
   return (
